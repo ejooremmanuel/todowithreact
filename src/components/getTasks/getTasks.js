ad9a2b7ca8ad149b78de24e6f0580e645342ec2d
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2021-11-05 21:42:37 +0100

    fixing the incessant state update

--- src/components/getTasks/getTasks.js
+++ src/components/getTasks/getTasks.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Delete, Edit } from "@material-ui/icons";
 import React, { useEffect, useContext } from "react";
 import { allTasks } from "./taskCall";
