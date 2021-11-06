5dee5b979de340d2d13abc1dd6836e516380d22e
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2021-11-06 19:38:44 +0100

    new

--- src/components/pages/Trash.js
+++ src/components/pages/Trash.js
@@ -1,19 +1,11 @@
 import React from "react";
 import Sidebar from "../sidebar/Sidebar";
 import Wrapper from "../pages/trashWrapper/wrapper/Wrapper";
-import Home from "../home/Home";
 const Trash = () => {
-  const user = JSON.parse(localStorage.getItem("user"));
   return (
     <div>
-      {user ? (
-        <>
-          <Wrapper />
-          <Sidebar />
-        </>
-      ) : (
-        <Home />
-      )}
+      <Sidebar />
+      <Wrapper />
     </div>
   );
 };
--- src/components/pages/userpage/Userpage.js
+++ src/components/pages/userpage/Userpage.js
@@ -1,19 +1,11 @@
 import React from "react";
 import Wrapper from "../../default/Wrapper";
 import Sidebar from "../../sidebar/Sidebar";
-import Home from "../../home/Home";
 const Userpage = () => {
-  const user = JSON.parse(localStorage.getItem("user"));
   return (
     <div>
-      {user ? (
-        <>
-          <Wrapper />
-          <Sidebar />
-        </>
-      ) : (
-        <Home />
-      )}
+      <Wrapper />
+      <Sidebar />
     </div>
   );
 };
