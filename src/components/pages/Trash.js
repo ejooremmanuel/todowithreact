4c9ae65af9bcb9a0862d5be0173931e05c82852b
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2021-11-06 18:01:56 +0100

    fixing some changes

--- src/components/pages/Trash.js
+++ src/components/pages/Trash.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Sidebar from "../sidebar/Sidebar";
 import Wrapper from "../pages/trashWrapper/wrapper/Wrapper";
+import Home from "../home/Home";
 const Trash = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
   return (
     <div>
-      <Sidebar />
-      <Wrapper />
+      {user ? (
+        <>
+          <Wrapper />
+          <Sidebar />
+        </>
+      ) : (
+        <Home />
+      )}
     </div>
   );
 };
--- src/components/pages/userpage/Userpage.js
+++ src/components/pages/userpage/Userpage.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Wrapper from "../../default/Wrapper";
 import Sidebar from "../../sidebar/Sidebar";
+import Home from "../../home/Home";
 const Userpage = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
   return (
     <div>
-      <Wrapper />
-      <Sidebar />
+      {user ? (
+        <>
+          <Wrapper />
+          <Sidebar />
+        </>
+      ) : (
+        <Home />
+      )}
     </div>
   );
 };
